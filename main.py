11358684d21c8346a48b3a8331fc6ad80c25d1db
Shuvo Nasir <shuvonasir028@gmail.com>
2021-08-18 02:58:39 +0600

    setup twilio to send notification to the user

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
+from twilio.rest import Client
 
 # Constants
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
 API_KEY = "6bd6ded191e85182ba748e6b16967fa5"
+ACCOUNT_SID = "ACf54a9c5f5c62fd561e2f5ccfd18aa36a"
+AUTH_TOKEN = "5dea37ebedea366f69b0d481b5434df2"
+TWILLIO_PHONE_NO = "+18503184267"
+RECEIVER = "+8801701340839"
 
 weather_params = {
     "lat": 23.7104,
@@ -19,4 +24,12 @@ weather_data = connection.json()
 hours = weather_data["hourly"][:12]
 weather_codes = [each_hour["weather"][0]["id"] for each_hour in hours]
 if any(weather_code < 600 for weather_code in weather_codes):
-    print("Bring an umbrella!")
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = client.messages \
+        .create(
+            body="It's going to rain today. Remember to bring an ☂️ when you go out.",
+            from_=TWILLIO_PHONE_NO,
+            to=RECEIVER
+        )
+
+    print(message.status)
